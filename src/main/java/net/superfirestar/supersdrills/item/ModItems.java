f13fb4e8f4f412e021e4a73b9513a9b4d82e11ef
isakh <isak.holmquist@outlook.com>
2025-03-28 17:45:35 +0100

    steel item added

---
+++ src/main/java/net/superfirestar/supersdrills/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.superfirestar.supersdrills.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.superfirestar.supersdrills.SupersDrills;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SupersDrills.MOD_ID);
+
+    public static final RegistryObject<Item> Steel = ITEMS.register("steel",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+
+}
